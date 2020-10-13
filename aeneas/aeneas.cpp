8e86d19290e3066fc3d034c3cc85ed9e26b25736
Ilias Fiotakis <iliasfio@gmail.com>
2020-10-14 01:30:49 +0300

    Initial commit

---
+++ aeneas/aeneas.cpp
@@ -0,0 +1,210 @@
+// Input
+//
+// First input is a single int that contains
+//the distance between the center and each letter 1, 100 range
+//
+// Next up we have the letters with a number in degrees
+// withc counterclockwise from the x-axis tells as where
+// the letters are 0.0, 360.0 range
+//
+// Last we have a phrase in english with max 10 ^ 6
+// If it has special chars -> ignore them
+// small letters -> convert to Capital
+//
+// Output
+//
+// A intiger which is the length in cm of the thread needed to
+// express the message given in the input as stated above as a decimal, rounded up
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+vector<double> convertInputStringToDegrees(string, double*);
+int findElements(int, string);
+int nthSubstr(int, const string&, const string&);
+int computeFinalDistance(int, vector<double>);
+double computeDistanceOfTwoPoints(int, double, double);
+double calculateDegreesBetweenTwoNodes(double, double);
+
+// function for conbersions
+int stringToInt(string);
+double stringToDouble(string);
+
+int main(int argc,const char *argv[])
+{
+
+	string line,lin;
+	lin = "";
+	int numOfIterations =26; 
+
+    // Fetch the data from our the input file
+    // Store the data in the string called lin
+	ifstream file("input.txt");
+	if (file.is_open())
+    {
+    while(getline(file, line))
+        {
+            lin = lin + line + "\n";
+        }
+        file.close();
+    }
+
+    // Find and store the radius from the lin string to the radius variable
+
+    //For testing purposes
+    findElements(numOfIterations,lin);
+
+	return 0;
+}
+
+// This function replaces all the non alphabetic chars of the string to nothing
+// And 
+// Returns an array of the degrees that corespond with the chars in the string
+vector<double> convertInputStringToDegrees(string inputLine, double* degrees){
+
+    vector<double> d;
+    int j = 0;
+    
+    // Iterate through the input string and
+    // Convert special chars to ''
+    // Store the degrees in the d array
+    for (int i = 0; i < inputLine.size(); ++i) {
+        if ((inputLine[i] >= 'a' && inputLine[i] <= 'z') || (inputLine[i] >= 'A' && inputLine[i] <= 'Z')) {
+            if(inputLine[i] >= 'a' && inputLine[i] <= 'z'){
+                int tmp = int(inputLine[i]) - 97;
+                d.push_back(degrees[tmp]);
+                j++;
+            }else{
+                int tmp = int(inputLine[i]) - 65;
+                d.push_back(degrees[tmp]);
+                j++;
+            }
+        }
+    }
+    return d;
+}
+
+double calculateDegreesBetweenTwoNodes(double firstDegree, double secondDegree){
+    double degreeBetweenTwoNodes = 0.0;
+
+    degreeBetweenTwoNodes = secondDegree - firstDegree;
+    if(degreeBetweenTwoNodes < 0){
+        degreeBetweenTwoNodes = -degreeBetweenTwoNodes;
+    }
+    if(degreeBetweenTwoNodes > 180){
+        degreeBetweenTwoNodes = 360 - degreeBetweenTwoNodes;
+    }
+    return degreeBetweenTwoNodes;
+}
+
+double computeDistanceOfTwoPoints(int radius, double firstDegree, double secondDegree){
+    double degreesBetweenTwoNodes, distanceBetweenTwoNodes = 0.0;
+
+    degreesBetweenTwoNodes = calculateDegreesBetweenTwoNodes(firstDegree, secondDegree);
+    degreesBetweenTwoNodes = (180 - degreesBetweenTwoNodes)/2;
+    degreesBetweenTwoNodes = degreesBetweenTwoNodes*3.14159/180;
+    distanceBetweenTwoNodes = 2*radius*cos(degreesBetweenTwoNodes);
+    if(distanceBetweenTwoNodes < 0){
+        distanceBetweenTwoNodes = -distanceBetweenTwoNodes;
+    }
+    return distanceBetweenTwoNodes;
+}
+
+int computeFinalDistance(int radius, vector<double> degrees){
+
+    double final_sum, distanceBetweenTwoNodes = 0.0;
+
+    // Take our degrees vector/ array
+    // Iterate through the degrees inside it
+    // We need the length of the degrees array
+    for(int i = 0; i < degrees.size() - 1; i++){
+        if(degrees[i + 1] != degrees[i]){
+            // Take i and (i + 1) and find the distance between them
+            distanceBetweenTwoNodes = computeDistanceOfTwoPoints(radius, degrees[i], degrees[i + 1]);
+            // Add the distance to our final_sum
+            final_sum += distanceBetweenTwoNodes;
+        }else continue;
+    }
+    cout << "Final sum: " << final_sum << endl;
+    final_sum = ceil(final_sum);
+    return final_sum;
+}
+
+// We need to find if there's a loop
+int nthSubstr(int n, const string& s, const string& p) {
+   string::size_type i = s.find(p);     // Find the first occurrence
+   int j;
+   for (j = 1; j < n && i != string::npos; ++j){
+      i = s.find(p, i+1); // Find the next occurrence
+   }
+   if (j == n){
+     return(i);
+   }
+   else{
+     return(-1);
+   }
+}
+
+// Convert string to int
+int stringToInt(string s)
+{
+	int newint = 0;
+	stringstream string1(s);
+	string1 >> newint; 
+	return newint;
+}
+
+// Convert string to double
+double stringToDouble(string s)
+{
+	double newdouble = 0.0;
+	newdouble = atof(s.c_str());
+	return newdouble;
+}
+
+int findElements(int numOfIterations, string inputs)
+{
+	int m = 0;
+	double degrees[numOfIterations];
+
+	for (int i = 1; i <= numOfIterations; i++)
+	{
+		string s = inputs.substr(nthSubstr(m+1, inputs, "\n") + 1,nthSubstr(m+2, inputs, "\n") - nthSubstr(m+1, inputs, "\n"));
+		m = m+1;
+		string letter = s.substr(0,2);
+		degrees[i-1] = stringToDouble(s.substr(nthSubstr(1,s," "))); 
+	}
+	string testInputString = inputs.substr(nthSubstr(numOfIterations+1,inputs,"\n")+1,nthSubstr(numOfIterations+2,inputs,"\n")-nthSubstr(numOfIterations+1,inputs,"\n"));
+    testInputString = testInputString.substr(0,testInputString.find_first_of("\n"));
+    cout << "Input string: " << testInputString << endl;
+
+    string radiusString = inputs.substr(0,inputs.find_first_of("\n")); 
+    int radius = stringToInt(radiusString); 
+    cout << "radius: " << radius << endl;
+
+    vector<double> newDegrees;
+    newDegrees = convertInputStringToDegrees(testInputString, degrees);
+
+
+    double distance = 0.0;
+
+    for(int i = 0; i < newDegrees.size() - 1; i++){
+        distance = computeDistanceOfTwoPoints(52, newDegrees[i], newDegrees[i + 1]);
+        cout << "The distance: " << distance << endl;
+    }
+
+    int finalDistance = computeFinalDistance(radius, newDegrees);
+    finalDistance += radius;
+    cout << "The final score: " << finalDistance << endl;
+
+	return finalDistance;
+}
+
---
+++ blackgate/Blackgate-Penitentiary.cpp
@@ -0,0 +1,180 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+// A utility function to swap two elements 
+void swap(int* a, int* b) 
+{ 
+    int t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
+  
+/* This function takes last element as pivot, places 
+   the pivot element at its correct position in sorted 
+    array, and places all smaller (smaller than pivot) 
+   to left of pivot and all greater elements to right 
+   of pivot */
+int partition (int arr[], int low, int high) 
+{ 
+    int pivot = arr[high];    // pivot 
+    int i = (low - 1);  // Index of smaller element 
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // If current element is smaller than or 
+        // equal to pivot 
+        if (arr[j] <= pivot) 
+        { 
+            i++;    // increment index of smaller element 
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+} 
+  
+/* The main function that implements QuickSort 
+ arr[] --> Array to be sorted, 
+  low  --> Starting index, 
+  high  --> Ending index */
+void quickSort(int arr[], int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        /* pi is partitioning index, arr[p] is now 
+           at right place */
+        int pi = partition(arr, low, high); 
+  
+        // Separately sort elements before 
+        // partition and after partition 
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+    } 
+}
+
+// Convert string to int
+int stringToInt(std::string s)
+{
+	int newint = 0;
+	std::stringstream string1(s);
+	string1 >> newint; 
+	return newint;
+}
+
+// Convert string to double
+double stringToDouble(std::string s)
+{
+	double newdouble = 0.0;
+	newdouble = atof(s.c_str());
+	return newdouble;
+}
+
+// We need to find if there's a loop
+int nthSubstr(int n, const string& s, const string& p) {
+	// Find the first occurrence
+	string::size_type i = s.find(p);
+	int j;
+	for (j = 1; j < n && i != string::npos; ++j){
+		i = s.find(p, i+1); // Find the next occurrence
+	}
+	if (j == n){
+		return(i);
+	}
+	else{
+		return(-1);
+	}
+}
+
+bool compareLengthOfStrings(string a, string b){
+	return a < b;
+}
+
+// Sort alphabatically
+vector<string> alphabaticallySort(vector<string> a){
+	int n=a.size();
+	sort(a.begin(),a.end(),compareLengthOfStrings);
+	return a;
+}
+
+// Read the inputs from the input file 
+// And return the output that we need
+void readInputs(string inputs, int numberOfPeople){
+	int m = 0, l = 0;
+    int heights[numberOfPeople];
+	vector<string> names,subStrings;
+
+	// Find the heights of the people through our file
+	for(int i = 1; i <= numberOfPeople; i++){
+		string s = inputs.substr(nthSubstr(m + 1, inputs, "\n") + 1, nthSubstr(m + 2, inputs, "\n") - nthSubstr(m + 1, inputs, "\n"));
+		m = m + 1;
+		subStrings.push_back(s);
+		heights[i - 1] = (stringToDouble(s.substr(nthSubstr(1, s, " "))));
+	}
+    
+	// Sort the heights of the people 
+    quickSort(heights, 0, numberOfPeople - 1);
+
+    for(int i = 1; i <= numberOfPeople; i++){
+		if(heights[i - 1] != heights[i]){
+            for(int k = 0; k < numberOfPeople; k++){
+
+                string s = subStrings[k];
+                string t = to_string(heights[i - 1]);
+                size_t pos = s.find(t);
+
+                if(pos != string::npos){
+                    names.push_back(subStrings[k].substr(0,pos));
+                }
+            }
+
+            l++;
+            names = alphabaticallySort(names);
+
+            for(int i = 0; i <= names.size() - 1; i++){
+                cout << names[i];
+            }
+
+            int m = 0;
+
+            if(names.size() < 2){
+                cout << l << " " << l;
+            }else{
+                m = names.size() - 1;
+                cout<< l << " " << l + m;
+            }
+
+            names.clear();
+            l = l + m;
+            cout << endl;
+        }
+    }
+}
+
+
+int main(int argc,const char *argv[]){
+	string line, lin;
+	ifstream file("input.txt");
+	lin = "";
+
+	if (file.is_open()){
+        while(getline(file, line)){
+            lin = lin + line + "\n";
+        }
+        file.close();
+    }
+
+    string s = lin.substr(0, lin.find_first_of("\n")); 
+    int numberOfPeople = stringToInt(s); 
+    readInputs(lin, numberOfPeople);
+
+	return 0;
+}
