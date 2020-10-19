64c2c0df42acf38ab0b76bd715e00ec95b367cda
Ilias Fiotakis <iliasfio@gmail.com>
2020-10-20 02:30:17 +0300

    Added batbear and running stairs

---
+++ runningStairs/running.py
@@ -0,0 +1,11 @@
+numOfIterations = int(raw_input())
+
+for n in range(numOfIterations):
+    first = 1
+    second = 1
+    numStairs = int(raw_input())
+    for i in range(numStairs - 1):
+        current = first + second
+        first = second
+        second = current
+    print second
